e26602e702fdeeb2a9fe329e4f3b340399a70262
prasadkukde <103832580+prasadkukde@users.noreply.github.com>
2022-08-20 13:54:01 +0530

    Update digit_recognition.py

--- digit_recognition.py
+++ digit_recognition.py
@@ -15,8 +15,13 @@ x_train_flattened = x_train.reshape(len(x_train),28*28)
 x_test_flattened = x_test.reshape(len(x_test),28*28)
 # print(x_train_flattened.shape)
 
-x_train_flattened = x_train_flattened/255;
+x_train_flattened = x_train_flattened/255
 x_test_flattened = x_test_flattened/255
+
+random_image = x_test_flattened[0].reshape(28,28)
+plt.imshow(random_image, cmap = matplotlib.cm.binary)
+print(y_train[0])
+
 
 model = keras.Sequential([keras.layers.Dense(10, input_shape=(784,),activation = 'sigmoid')])
 
